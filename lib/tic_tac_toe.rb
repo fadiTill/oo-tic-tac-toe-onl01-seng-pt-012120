620192fdcad1b73090d3830f0ed8797fc34a7cda
fadimata tillman <fadi.tillman@gmail.com>
2020-02-13 18:18:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,3 +17,11 @@ WIN_COMBINATIONS = [
     @board = Array.new(9, " ")
   end
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
